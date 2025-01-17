2eaa69e7f826a896c1b1ab5a573239da02e52a3d
oliver po <oliver.po2882@gmail.com>
2025-01-17 14:09:12 +1000

    use batch

--- logistic_regression/main.py
+++ logistic_regression/main.py
@@ -3,18 +3,19 @@ import pandas as pd
 
 
 class LogisticRegression:
-    def __init__(self, learning_rate: float, dims: int | None = None):
+    def __init__(self, learning_rate: float, tolerance: float, dims: int | None = None):
         self.dims = dims
-        self.intercept = np.random.rand()
-        self.slope = np.random.rand(dims) if dims is not None else np.random.rand()
+        self.intercept = 0
+        self.slope = np.zeros(dims) if dims is not None else 0
         self.learning_rate = learning_rate
+        self.tolerance = tolerance
 
     @staticmethod
     def cost(predicted: np.ndarray | float, actual: np.ndarray | float) -> float:
         predicted = np.asarray(predicted)
         actual = np.asarray(actual)
-        
-        epsilon = 1e-9
+
+        epsilon = 1e-15
         predicted = np.clip(predicted, epsilon, 1 - epsilon)
         return -np.mean(
             actual * np.log(predicted) + (1 - actual) * np.log(1 - predicted)
@@ -22,24 +23,21 @@ class LogisticRegression:
 
     @staticmethod
     def cost_derivative_slope(
+        slope: np.ndarray | float,
         inputs: np.ndarray | float,
         predicted: np.ndarray | float,
         actual: np.ndarray | float,
     ) -> np.ndarray:
-        inputs = np.asarray(inputs)
-        predicted = np.asarray(predicted)
-        actual = np.asarray(actual)
         # needs inputs too cause sigmoid is applied
-
-        return np.dot(inputs.T, (predicted - actual)) / (len(inputs) if inputs.ndim != 0 else 1)
+        return np.dot(inputs.T, (predicted - actual)).reshape(slope.shape) / (
+            inputs.shape[0] if inputs.ndim != 0 else 1
+        )
 
     @staticmethod
     def cost_derivative_intercept(
         predicted: np.ndarray | float,
         actual: np.ndarray | float,
     ) -> np.ndarray:
-        predicted = np.asarray(predicted)
-        actual = np.asarray(actual)
 
         return np.mean(predicted - actual)
 
@@ -61,40 +59,51 @@ class LogisticRegression:
             predicted, actual
         )
         self.slope -= self.learning_rate * self.cost_derivative_slope(
-            inputs, predicted, actual
+            self.slope, inputs, predicted, actual
         )
 
     def predict(self, inputs: np.ndarray | float):
-        linear = (
-            np.dot(self.slope, inputs) + self.intercept
-            if isinstance(inputs, np.ndarray)
-            else self.slope * inputs + self.intercept
-        )
+        linear = np.dot(inputs, self.slope) + self.intercept
+
         return LogisticRegression.sigmoid(linear)
 
-    def train(self, data: str, epochs: int):
+    def train(self, inputs, actual, epochs: int):
+        inputs = np.array(inputs)
+        actual = np.array(actual)
+
+        prev_loss = float("inf")
         for epoch in range(epochs):
             print(f"Epoch {epoch+1}")
-            df = pd.read_csv(data)
-            losses = []
-            for _, row in df.iterrows():
-                inputs = (
-                    row["inputs"]
-                    if self.dims is None
-                    else np.array([row[f"inputs_{i}"] for i in range(self.dims)])
-                )
+            predicted = self.predict(inputs).reshape(actual.shape)
+            self.gradient_descent(inputs, predicted, actual)
 
-                actual = row["actual"]
+            loss = self.cost(predicted, actual)
+            if abs(prev_loss - loss) < self.tolerance:
+                return
+            if loss > prev_loss:
+                self.learning_rate *= 0.5
+            prev_loss = loss
+            print(f"Loss: {np.mean(loss)}")
+    
+    def score(self, inputs, actual):
+        inputs = np.array(inputs)
+        actual = np.array(actual)
 
-                predicted = self.predict(inputs)
-                self.gradient_descent(inputs, predicted, actual)
-                loss = self.cost(predicted, actual)
-                losses.append(loss)
-            print(f"Average loss: {np.mean(losses)}")
+        predicted = self.predict(inputs).reshape(actual.shape)
+
+        return np.mean(predicted == actual)
 
 
 if __name__ == "__main__":
-    model = LogisticRegression(1e-5)
-    model.train("data2.csv", 10)
-    print(f"Prediction {model.predict(44)}")
-    print(f"Prediction {model.predict(3)}")
+
+    df = pd.read_csv("data.csv")
+    inputs = df.drop(["actual"], axis=1)
+    actual = df[["actual"]]
+
+    num_inputs = inputs.shape[1]
+
+    model = LogisticRegression(0.001, 0.0001, num_inputs)
+
+    model.train(inputs, actual, 100000)
+    print (f"Score: {model.score(inputs, actual)}")
+    print(f"Prediction {np.round(model.predict([4]))}")
